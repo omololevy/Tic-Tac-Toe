ff557801af86e7149b4e8e282545128e418fb727
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-25 10:52:44 +0300

    Input the content adding the main function

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 
 from player import Player
 
-player_obj=Player()
+user=Player()
 input_list=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def show_grid():
@@ -14,7 +14,8 @@ def show_grid():
     print("-"*5+"|"+"-"*5+"|"+"-"*5)
     print(f'  {input_list[6]}  |  {input_list[7]}  |  {input_list[8]}  ')
 
-def check_winner():
+def win_state():
+    """Defines all possible winning alignment of the columns and rows on the board"""
     if input_list[0]==input_list[1] and input_list[1]==input_list[2]:
         if input_list[0] != " ":
             return True
@@ -51,16 +52,18 @@ def check_winner():
         return False
 
 def main():
+    print("Welcome to the game.")
+    player1 = input("Player 1 enter your name.")
+    player2 = input("Player 2 enter your name.")
   
-    which_player=True #Checks which player should play.
-
-    is_stalemate= True #This checks for a draw
+    which_player=True 
+    
+    is_stalemate= True 
     
         
     print("     TIC-TAC_TOE")
-    print("This is how the grid looks like")
-    print("\n")
-
+    print("This is how the board looks like\n")
+    
     print(" 1   | 2   | 3   ")
     print("-"*5+"|"+"-"*5+"|"+"-"*5)
     print(" 4   | 5   | 6   ")
@@ -69,38 +72,71 @@ def main():
     print("\n")
 
     print("     START")
-    while len(player_obj.moves_available)>0:
+    while len(user.moves_available)>0:
         value_error=False
         
         if which_player==True:
-            print("\nPlayer 1:")
+            print(f"\n{player1}:")
             print("Available moves:")
-            player_obj.show_av_moves()
+            user.show_av_moves()
             show_grid() #prints the board on the terminal 
             print("Player 1, make a move")
             
             try:
-                x=int(input()) #Accepts only input of type integer
+                slot=int(input()) #Accepts only input of type integer
             except ValueError:
                 value_error=True
                 print("Kindly use numbers 1-9!!") #What is to be displayed once the input iss wrong
             
             if value_error==False:
-                if player_obj.validate_move(x):
-                    input_list[x-1]="X"
-                    player_obj.move_made(x)
+                if user.validate_move(slot):
+                    input_list[slot-1]="X"
+                    user.move_made(slot)
                     which_player=False
                     
-                    if check_winner():
+                    if win_state():
                         show_grid()
 
-                        print("\n-----(( PLAYER ONE WINS ))-----\n               END")                    
+                        print(f"\n----- {player1} WINS! -----\n               END")                    
                         
                         is_stalemate=False
                         break
                 else:
                     print("******Invalid move, try again******")
                     which_player=True   
+
+        else:
+            print(f"\n{player2}:")
+            print("Available moves:")
+            user.show_av_moves()
+            show_grid()
+            print("Player 2 make a move.")
+            try:
+                slot=int(input())
+            except ValueError:
+                value_error=True
+                print("Kindly use numbers 1-9!!")
+            
+            if value_error==False:
+                if user.validate_move(slot):
+                    input_list[slot-1]="O"
+                    user.move_made(slot)
+                    which_player=True
+
+                    if win_state():
+                        show_grid()
+                        print(f"\n-----{player2} WINS! -----")                    
+                        print("              END")
+                        is_stalemate=False
+                        break
+                else:
+                    print("Invalid Move! Repeat.")
+                    which_player=False
+
+    if is_stalemate:
+        show_grid()
+        print("\nThe game has ended in a draw\n      END")
+    
 
 if __name__ == "__main__":
     main ()
