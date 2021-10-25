506c4ac18e1a3148d398839ac7b9c4af586bb1e7
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-25 10:54:08 +0300

    Change the variables name to a more understandable ones

--- player.py
+++ player.py
@@ -1,14 +1,17 @@
 
 class Player:
-    moves_available= [1,2,3,4,5,6,7,8,9]
+    moves_available = [1,2,3,4,5,6,7,8,9]
     
     def show_av_moves(self):
+        """This shows all the available moves before a player makes a move"""
         print(self.moves_available)   
 
     def move_made(self, move):
+        """This eliminates the already made moves from the moves_available list"""
         Player.moves_available.remove(move) 
 
     def validate_move(self, move):
+        """checks if a player has made a valid move"""
         if move in Player.moves_available:
             return True
         else:
