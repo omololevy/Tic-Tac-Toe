c33fb07350c620c6f4f5f7542d199c48ed38fe56
Levy Omolo <levy.omolo@student.moringaschool.com>
2021-10-25 06:31:15 +0300

    Define the winning states of the game

--- run.py
+++ run.py
@@ -13,3 +13,39 @@ def show_grid():
     print(f'  {input_list[3]}  |  {input_list[4]}  |  {input_list[5]}  ')
     print("-"*5+"|"+"-"*5+"|"+"-"*5)
     print(f'  {input_list[6]}  |  {input_list[7]}  |  {input_list[8]}  ')
+
+def check_winner():
+    if input_list[0]==input_list[1] and input_list[1]==input_list[2]:
+        if input_list[0] != " ":
+            return True
+
+    elif input_list[3]==input_list[4] and input_list[4]==input_list[5]:
+        if input_list[3] != " ":
+            return True
+
+    elif input_list[6]==input_list[7] and input_list[7]==input_list[8]:
+        if input_list[6] != " ":
+            return True
+
+    elif input_list[0]==input_list[3] and input_list[3]==input_list[6]:
+        if input_list[0] != " ":
+            return True
+
+    elif input_list[1]==input_list[4] and input_list[4]==input_list[7]:
+        if input_list[1] != " ":
+            return True
+
+    elif input_list[2]==input_list[5] and input_list[5]==input_list[8]:
+        if input_list[2] != " ":
+            return True
+
+    elif input_list[0]==input_list[4] and input_list[4]==input_list[8]:
+        if input_list[0] != " ":
+            return True
+
+    elif input_list[2]==input_list[4] and input_list[4]==input_list[6]:
+        if input_list[2] != " ":
+            return True
+
+    else:
+        return False
